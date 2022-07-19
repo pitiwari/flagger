ac9ddd2d1fd05f413ad7e4a5f6df961537dcb2be
piyush tiwari <ptiwari@roku.com>
2022-07-19 12:42:42 -0700

    add webhook changes

--- pkg/controller/scheduler.go
+++ pkg/controller/scheduler.go
@@ -354,8 +354,9 @@ func (c *Controller) advanceCanary(name string, namespace string) {
 
 	// scale canary to zero if promotion has finished
 	if cd.Status.Phase == flaggerv1.CanaryPhaseFinalising {
-		fmt.Println("Canary Sleep Over.....")
+		c.recordEventInfof(cd, "Canary Sleep Over")
 		time.Sleep(100 * time.Second)
+		c.runPreScaleToZeroCanaryRolloutHooks(cd, flaggerv1.CanaryPhaseSucceeded)
 		if scalerReconciler != nil {
 			if err := scalerReconciler.PauseTargetScaler(cd); err != nil {
 				c.recordEventWarningf(cd, "%v", err)
